cfbc3c22be8556be13f7755f32373185b44ecea2
Arnon Moscona <arnon.moscona@syapse.com>
2020-05-11 10:56:21 -0700

    added some jupyter notebooks collected from other repos

---
+++ other_people_work/darwin-s-finches/Various Functions.py
@@ -0,0 +1,46 @@
+def bootstrap_replicate_1d(data, func):
+    return func(np.random.choice(data, size=len(data)))
+
+def draw_bs_reps(data, func, size=1):
+    """Draw bootstrap replicates."""
+
+    # Initialize array of replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_replicates[i] = bootstrap_replicate_1d(data,func,size)
+
+    return bs_replicates
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds,size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x,bs_y,1)
+
+    return bs_slope_reps, bs_intercept_reps
+
+def pearson_r(x,y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat = np.corrcoef(x,y)
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
+
+    # Compute Pearson correlation coefficient for I. versicolor: r
+    r = pearson_r(versicolor_petal_length,versicolor_petal_width)
+
+    # Print the result
+    print(r)
